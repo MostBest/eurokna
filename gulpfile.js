26879e5d8152956a72f8204310593e99f83cb50b
MostBest <info@mostbest.ru>
2025-11-20 18:14:21 +0300

    create gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
 
 var paths = {
-  style: {
-    src: 'src/style/**/*.*',
+  styles: {
+    src: 'src/styles/**/*.*',
+    sass: 'src/sass/**/*.*',
     dest: 'assets/css'
+  },
+  watching: {
+    css: 'src/sass/**/*.*'
   }
 }
 
+function css() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+exports.css = css;
+exports.watch = function () {
+  gulp.watch('')
+}
